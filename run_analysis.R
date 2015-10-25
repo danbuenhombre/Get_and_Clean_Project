349a46c6912bd88f10584714a8e322fb68326bba
= <hookem.goodman@gmail.com>
2015-10-25 09:48:02 -0600

    modified paths in code and final README edits

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,10 @@
 run_analysis <- function() {
 
-# set working dir
-# setwd("~/dataScience/getnclean/project/UCI HAR Dataset")
-
 # load dplyr library
 library(dplyr)
   
 # column names for X_train.txt and X_test.txt are in features.txt
-features <- read.table("./features.txt")
+features <- read.table("./UCI\ HAR\ Dataset/features.txt")
 colnames(features) <- c("featureID", "feature")
 
 # replace (), from column names
@@ -17,21 +14,21 @@ features$feature <- gsub("\\(", "-", features$feature)
 features$feature <- gsub(")", "", features$feature)
 
 # activities are listed in activity_labels.txt
-activities <- read.table("./activity_labels.txt")
+activities <- read.table("./UCI\ HAR\ Dataset/activity_labels.txt")
 colnames(activities) <- c("activityID", "activity")
 
 # read train data
-rawTrain <- read.table("./train/X_train.txt")
+rawTrain <- read.table("./UCI\ HAR\ Dataset/train/X_train.txt")
 colnames(rawTrain) <- features$feature
 # get only mean and std dev variables
 rawTrain <- rawTrain[,grep("[Mm]ean|[Ss]td", names(rawTrain))]
 
 # read subject list
-subjectTrain <- read.table("./train/subject_train.txt")
+subjectTrain <- read.table("./UCI\ HAR\ Dataset/train/subject_train.txt")
 colnames(subjectTrain) <- c("subjectID")
 
 # read labels
-labelTrain <- read.table("./train/y_train.txt")
+labelTrain <- read.table("./UCI\ HAR\ Dataset/train/y_train.txt")
 colnames(labelTrain) <- c("activityID")
 # merge label names on activityID
 labelTrain <- merge(labelTrain, activities)
@@ -40,17 +37,17 @@ labelTrain <- merge(labelTrain, activities)
 train <- cbind(rawTrain, subjectTrain, labelTrain)
 
 # read test data
-rawTest <- read.table("./test/X_test.txt")
+rawTest <- read.table("./UCI\ HAR\ Dataset/test/X_test.txt")
 colnames(rawTest) <- features$feature
 # get only mean and std dev variables
 rawTest <- rawTest[,grep("[Mm]ean|[Ss]td", names(rawTest))]
 
 # read subject list for test
-subjectTest <- read.table("./test/subject_test.txt")
+subjectTest <- read.table("./UCI\ HAR\ Dataset/test/subject_test.txt")
 colnames(subjectTest) <- c("subjectID")
 
 # read labels for test
-labelTest <- read.table("./test/y_test.txt")
+labelTest <- read.table("./UCI\ HAR\ Dataset/test/y_test.txt")
 colnames(labelTest) <- c("activityID")
 # merge label names on activityID
 labelTest <- merge(labelTest, activities)
